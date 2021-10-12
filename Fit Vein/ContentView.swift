3583feb616bd218beec3b22fb49a8c269ab63451
Vader20FF <229898@edu.p.lodz.pl>
2021-10-12 13:32:56 +0200

    Initial Commit

---
+++ Fit Vein/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fit Vein
+//
+//  Created by Łukasz Janiszewski on 12/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fit Vein/Fit_VeinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fit_VeinApp.swift
+//  Fit Vein
+//
+//  Created by Łukasz Janiszewski on 12/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Fit_VeinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
