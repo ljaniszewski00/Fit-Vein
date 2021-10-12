9813e167f745ae0f3953f3f1b8eeadcbca041d66
Vader20FF <229898@edu.p.lodz.pl>
2021-10-12 14:02:34 +0200

    - Integrated project with Google Firebase

--- Fit Vein/Fit_VeinApp.swift
+++ Fit Vein/Fit_VeinApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Fit_VeinApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
