ed1febc65d8f88c0c85147b5f9ee2433c5a898a1
Vader20FF <229898@edu.p.lodz.pl>
2021-10-25 23:07:55 +0200

    - Added ListView to WorkoutTabView - Divided WorkoutTabViews to some SubViews in order to increase code clarity - Now we can choose in ProfileView whether we want to see Health Data or Workouts Data and whether to see the second one like workout one by one in HorizontalPageTabView or to see all workouts in list with possibility to click one workout and see details.

---
+++ Fit Vein/Views/HealthTabView.swift
@@ -0,0 +1,83 @@
+//
+//  HealthTabView.swift
+//  Fit Vein
+//
+//  Created by Łukasz Janiszewski on 25/10/2021.
+//
+
+import SwiftUI
+
+struct HealthTabView: View {
+    @ObservedObject private var profileViewModel: ProfileViewModel
+    
+    private var dataImagesNames: [String] = ["flame.fill", "flame.fill", "flame.fill", "timer", "heart.fill"]
+    private var dataNames: [String] = ["Steps", "Calories", "Distance", "Workout Time", "Pulse"]
+    private var dataValues: [String] = ["3069", "234", "8.8", "1.5", "98"]
+    private var dataValuesUnits: [String] = ["", "", "km", "hours", ""]
+    
+    init(profileViewModel: ProfileViewModel) {
+        self.profileViewModel = profileViewModel
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let screenWidth = geometry.size.width
+            let screenHeight = geometry.size.height
+        
+            NavigationView {
+                ScrollView(.vertical) {
+                    LazyVGrid(columns: [GridItem(.flexible()),
+                                        GridItem(.flexible())], spacing: 0) {
+                        ForEach(0..<dataNames.count) { number in
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 25)
+                                    .frame(height: screenHeight * 0.2)
+                                    .foregroundColor([0, 3, 4].contains(number) ? .green : .none)
+                                
+                                VStack {
+                                    HStack {
+                                        Image(systemName: dataImagesNames[number])
+                                        Text(dataNames[number])
+                                            .fontWeight(.bold)
+                                        Spacer()
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    HStack {
+                                        Text(dataValues[number])
+                                            .font(.title)
+                                            .fontWeight(.bold)
+                                        Text(dataValuesUnits[number])
+                                    }
+                                    
+                                    Spacer()
+                                }
+                                .foregroundColor([0, 3, 4, 7, 8].contains(number) ? .none : .green)
+                                .padding()
+                            }
+                            .padding()
+                        }
+                    }
+                }
+                .navigationTitle("Health Data")
+                .navigationBarHidden(false)
+            }
+        }
+    }
+}
+
+struct HealthTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        let profileViewModel = ProfileViewModel(forPreviews: true)
+        ForEach(ColorScheme.allCases, id: \.self) { colorScheme in
+            ForEach(["iPhone XS MAX", "iPhone 8"], id: \.self) { deviceName in
+                
+                HealthTabView(profileViewModel: profileViewModel)
+                    .preferredColorScheme(colorScheme)
+                    .previewDevice(PreviewDevice(rawValue: deviceName))
+                    .previewDisplayName(deviceName)
+            }
+        }
+    }
+}
---
+++ Fit Vein/Views/WorkoutTabView.swift
@@ -0,0 +1,261 @@
+//
+//  WorkoutTabView.swift
+//  Fit Vein
+//
+//  Created by Łukasz Janiszewski on 25/10/2021.
+//
+
+import SwiftUI
+
+struct WorkoutTabView: View {
+    @ObservedObject private var profileViewModel: ProfileViewModel
+    @State private var howToDisplay = 0
+    
+    init(profileViewModel: ProfileViewModel) {
+        self.profileViewModel = profileViewModel
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let screenWidth = geometry.size.width
+            let screenHeight = geometry.size.height
+            
+            NavigationView {
+                Group {
+                    if howToDisplay == 0 {
+                        WorkoutTabViewWindows(profileViewModel: profileViewModel)
+                    } else {
+                        WorkoutTabViewList(profileViewModel: profileViewModel)
+                    }
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Picker("", selection: $howToDisplay) {
+                            Image(systemName: "squareshape.split.2x2").tag(0)
+                            Image(systemName: "list.bullet").tag(1)
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct WorkoutTabViewWindows: View {
+    @ObservedObject private var profileViewModel: ProfileViewModel
+    
+    init(profileViewModel: ProfileViewModel) {
+        self.profileViewModel = profileViewModel
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let screenWidth = geometry.size.width
+            let screenHeight = geometry.size.height
+            
+            TabView {
+                ForEach(profileViewModel.workouts!) { workout in
+                    SingleWorkoutWindowView(workout: workout)
+                }
+            }
+            .tabViewStyle(.page)
+            .navigationTitle("Workouts")
+            .navigationBarHidden(false)
+        }
+    }
+}
+
+struct WorkoutTabViewList: View {
+    @ObservedObject var profileViewModel: ProfileViewModel
+    
+    init(profileViewModel: ProfileViewModel) {
+        self.profileViewModel = profileViewModel
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let screenWidth = geometry.size.width
+            let screenHeight = geometry.size.height
+            
+            List(profileViewModel.workouts!) { workout in
+                NavigationLink(destination: SingleWorkoutWindowView(workout: workout)
+                                .navigationTitle("Workout")
+                                .navigationBarHidden(false)) {
+                    HStack {
+                        Image(uiImage: UIImage(named: "sprint")!)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: screenWidth * 0.1, height: screenHeight * 0.1)
+                            .padding(.trailing)
+                        
+                        VStack {
+                            Text(workout.type)
+                                .font(.title3)
+                                .fontWeight(.bold)
+                            Text(getShortDate(longDate: workout.date))
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Workouts")
+            .navigationBarHidden(false)
+        }
+    }
+}
+
+struct SingleWorkoutWindowView: View {
+    private var workout: IntervalWorkout
+    
+    private var dataImagesNames: [String] = ["timer", "flame", "play.circle", "pause.circle", "123.rectangle"]
+    private var dataNames: [String] = ["Duration", "Calories", "Work Time", "Rest Time", "Series"]
+    private var dataValuesUnits: [String] = ["minutes", "cal", "km", "hours", ""]
+    
+    @Environment(\.colorScheme) var colorScheme
+    
+    init(workout: IntervalWorkout) {
+        self.workout = workout
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let screenWidth = geometry.size.width
+            let screenHeight = geometry.size.height
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 25).foregroundColor(Color(uiColor: UIColor.darkGray))
+                
+                VStack {
+                    VStack {
+                        HStack {
+                            VStack {
+                                Image(uiImage: UIImage(named: "sprint")!)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: screenWidth * 0.25, height: screenHeight * 0.3)
+                                
+                                Text("Image by freepik: www.freepik.com")
+                                    .font(.caption2)
+                                    .padding(.top, -screenHeight * 0.09)
+                                    .foregroundColor(Color(uiColor: UIColor.lightGray))
+                            }
+                            
+                                
+                            Spacer()
+                            
+                            VStack {
+                                Text("\(workout.type)")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.green)
+                                
+                                Text("\(getShortDate(longDate: workout.date))")
+                                    .font(.caption)
+                                    .foregroundColor(Color(uiColor: UIColor.lightGray))
+                                
+                                Text("Workout Details")
+                                    .font(.title3)
+                                    .foregroundColor(.green)
+                                    .padding(.top, screenHeight * 0.02)
+                                    .padding(.bottom, screenHeight * 0.01)
+                                
+                                Image(systemName: "arrow.down.circle.fill")
+                                    .foregroundColor(.white)
+                                    .font(.title2)
+                            }
+                            
+                            Spacer()
+                        }
+                    }
+                    .padding()
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 25).foregroundColor(.green)
+                        
+                        LazyVGrid(columns: [GridItem(.flexible()),
+                                            GridItem(.flexible())], spacing: 0) {
+                            
+                            ForEach(0..<dataNames.count) { number in
+                                HStack {
+                                    Image(systemName: dataImagesNames[number])
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: screenWidth * 0.06, height: screenHeight * 0.06)
+                                    
+                                    Spacer()
+                                    
+                                    VStack {
+                                        Spacer()
+                                        
+                                        HStack {
+                                            Text(dataNames[number])
+                                                .fontWeight(.bold)
+                                            
+                                            Spacer()
+                                        }
+                                        
+                                        HStack {
+                                            if number == 0 {
+                                                Text("\(Int(workout.duration))")
+                                                    .font(.title)
+                                                    .fontWeight(.bold)
+                                            } else if number == 1 {
+                                                Text("\(workout.calories)")
+                                                    .font(.title)
+                                                    .fontWeight(.bold)
+                                            } else if number == 2 {
+                                                Text("\(workout.workTime)")
+                                                    .font(.title)
+                                                    .fontWeight(.bold)
+                                            } else if number == 3 {
+                                                Text("\(workout.restTime)")
+                                                    .font(.title)
+                                                    .fontWeight(.bold)
+                                            } else if number == 4 {
+                                                Text("\(workout.series)")
+                                                    .font(.title)
+                                                    .fontWeight(.bold)
+                                            }
+                                            
+                                            Text(dataValuesUnits[number])
+                                            
+                                            Spacer()
+                                        }
+                                        
+                                        Spacer()
+                                    }
+                                }
+                                .padding()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct WorkoutTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        let profileViewModel = ProfileViewModel(forPreviews: true)
+        ForEach(ColorScheme.allCases, id: \.self) { colorScheme in
+            ForEach(["iPhone XS MAX", "iPhone 8"], id: \.self) { deviceName in
+                
+                WorkoutTabView(profileViewModel: profileViewModel)
+                    .preferredColorScheme(colorScheme)
+                    .previewDevice(PreviewDevice(rawValue: deviceName))
+                    .previewDisplayName(deviceName)
+                
+                WorkoutTabViewWindows(profileViewModel: profileViewModel)
+                    .preferredColorScheme(colorScheme)
+                    .previewDevice(PreviewDevice(rawValue: deviceName))
+                    .previewDisplayName(deviceName)
+                
+                WorkoutTabViewList(profileViewModel: profileViewModel)
+                    .preferredColorScheme(colorScheme)
+                    .previewDevice(PreviewDevice(rawValue: deviceName))
+                    .previewDisplayName(deviceName)
+            }
+        }
+    }
+}
